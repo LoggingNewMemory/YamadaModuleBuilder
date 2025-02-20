f5a8b365b452ee3bd4f5e1602fbf710c236c4338
Kanagawa Yamada <83739103+LoggingNewMemory@users.noreply.github.com>
2025-02-20 21:46:46 +0700

    Update

---
+++ Modules/Example/Example.sh
@@ -0,0 +1,13 @@
+// This is a function, so you don't need to chmod 644 everytime
+
+tweak() {
+	if [ -f $2 ]; then
+		chmod 644 $2 >/dev/null 2>&1
+		echo $1 >$2 2>/dev/null
+		chmod 444 $2 >/dev/null 2>&1
+	fi
+}
+
+// you can use this like this
+
+tweak <your modification>
