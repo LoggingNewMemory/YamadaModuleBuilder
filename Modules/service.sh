066bfb86566061f9077b2274e8aad439df0d369b
Kanagawa Yamada <83739103+LoggingNewMemory@users.noreply.github.com>
2025-02-21 10:09:32 +0700

    Update service.sh

--- Modules/service.sh
+++ Modules/service.sh
@@ -1,6 +1,6 @@
 while [ -z "$(getprop sys.boot_completed)" ]; do
-	sleep 30
-	# Modify by yourself
-    /data/adb/modules/[ID Module]/Example/Example.sh
+sleep 10
+# Modify by yourself
+sh /data/adb/modules/[ID Module]/Example/Example.sh
 done
 
